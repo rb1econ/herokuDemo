7b1ea8339902bd84f98c7a4dcc4b9ce549fc313f
Ryan budny <rb1econ@gmail.com>
2015-02-20 09:33:15 -0700

    a commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+var port = process.env.PORT || 6324;
+var server = app.listen(port, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
